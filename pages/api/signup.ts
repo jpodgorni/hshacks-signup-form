4a4f8df747021d35df5e16bf681975977cc0f96d
jpodgorni <jakubjpodgorni@gmail.com>
2024-10-01 23:20:35 -0500

    Update signup.ts
    statuses all stored in errors array

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -2,51 +2,54 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        let firstName: string = req.body.firstName();
-        let lastName: string = req.body.lastName();
-        let email: string = req.body.email();
-        let laptop: boolean = req.body.laptop();
-        let experience: string = req.body.experience();
-        let grade: number = req.body.grade();
-        let shirt: string = req.body.shirt();;
-        let diet: string = req.body.diet();;
-        let other: string = req.body.other();;
+        let firstName: string = req.body.firstName;
+        let lastName: string = req.body.lastName;
+        let email: string = req.body.email;
+        let laptop: boolean = req.body.laptop;
+        let experience: string = req.body.experience;
+        let grade: number = req.body.grade;
+        let shirt: string = req.body.shirt;
+        let diet: string = req.body.diet;
+        let other: string = req.body.other;
+        let errors = []; //grouping: names, email, long paragraphs, button choices
         const names: string[] = [firstName, lastName];
         const explains: string[] = [experience, diet, other];
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2-4}$/;
-        const namePattern = /^[a-zA-Z]{2-25}$/;
+        const choices: any[] = [laptop, grade, shirt];
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const namePattern = /^[a-zA-Z]{2,25}$/;
         const explainPattern = /^[a-zA-Z0-9\(\)._ -]+$/;
 
-        if (grade == null || laptop == null || shirt == null) {
-            res.status(400).json({ message: "Bad Request" })
-        } else {
-            res.status(200).json({ message: "OK" })
+        for (var s of names) {
+            if (namePattern.test(s)) {
+                errors.push(res.status(200).json({ message: "OK" }));
+            } else {
+                errors.push(res.status(400).json({ message: "Bad Request" }));
+            }
         }
 
         if (emailPattern.test(email)) {
-            res.status(200).json({ message: "OK" })
+            errors.push(res.status(200).json({ message: "OK" }));
         } else {
-            res.status(400).json({ message: "Bad Request" })
+            errors.push(res.status(400).json({ message: "Bad Request" }));
         }
 
-        for (var s in names) {
-            if (namePattern.test(s)) {
-                res.status(200).json({ message: "OK" })
-            } else {
-                res.status(400).json({ message: "Bad Request" })
-            }
-        }
-
-        for (var s in explains) {
-            if (s.length > 250) {
-                res.status(400).json({ message: "Bad Request" })
-            }
+        for (var s of explains) {
             if (explainPattern.test(s)) {
-                res.status(200).json({ message: "OK" })
+                errors.push(res.status(200).json({ message: "OK" }));
             } else {
-                res.status(400).json({ message: "Bad Request" })
+                errors.push(res.status(400).json({ message: "Bad Request" }));
             }
         }
+
+        for (let i = 0; i < 3; i++) {
+            if (choices[i] != null) {
+                errors.push(res.status(200).json({ message: "OK" }));
+            } else {
+                errors.push(res.status(400).json({ message: "Bad Request" }));
+            }
+        }
+
+        return errors;
     } catch (err) {
         return res.status(500).json({ message: "Server Error" })
     }
