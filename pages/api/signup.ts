948e1b595658f546a4152faeb55c760ec50649f5
jpodgorni <jakubjpodgorni@gmail.com>
2024-10-22 22:36:00 -0500

    Update signup.ts

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method == "POST") {
-        return res.status(500).json({ message: "Request method is invalid" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method != "POST") {
+        return res.status(405).json({ message: "Request method is invalid" });
     }
     try {
         let {firstName, lastName, email, laptop, experience, grade, shirt, diet, other} = req.body;
@@ -10,9 +10,16 @@ export async function handler(req: NextApiRequest, res: NextApiResponse) {
         const names: string[] = [firstName, lastName];
         const explains: string[] = [experience, diet, other];
         const choices: any[] = [laptop, grade, shirt];
+        const combinedArr = names.concat(explains.concat(choices));
         const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const namePattern = /^[a-zA-Z\s'-]{2,25}$/;
-        const explainPattern = /^[a-zA-Z0-9\(\)._ -]+$/;
+        const explainPattern = /^[a-zA-Z0-9\s.,'!?\-()]{2,500}$/;
+
+        for (let i = 0; i < combinedArr.length; i++) {
+            if (combinedArr[i] == null) {
+                return res.status(400).json({ message: "Empty Input"});
+            }
+        }
 
         for (var s of names) {
             if (!namePattern.test(s)) {
@@ -37,7 +44,7 @@ export async function handler(req: NextApiRequest, res: NextApiResponse) {
         }
 
         if (errors.length > 0) {
-            return res.status(400).json({ message: "Errors" });
+            return res.status(400).json({ message: "Errors", details: errors });
         } else {
             return res.status(200).json({ message: "OK" });
         }
